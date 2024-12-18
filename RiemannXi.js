a02cc93f481045bf9023345f7239a1485ca25d94
YouSlovenia <loverussiamapping656@gmail.com>
2024-12-18 19:19:36 -0300

    Update RiemannXi.js

--- RiemannXi.js
+++ RiemannXi.js
@@ -23,12 +23,13 @@ var init = () => {
     ///////////////////
     // Regular Upgrades
 
-    // c1
+    // t
     {
-        let getDesc = (level) => "c_1=" + getC1(level).toString(0);
-        c1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(15, Math.log2(2))));
-        c1.getDescription = (_) => Utils.getMath(getDesc(c1.level));
-        c1.getInfo = (amount) => Utils.getMathTo(getDesc(c1.level), getDesc(c1.level + amount));
+        let getDesc = (level) => "t=" + getT(level).toString(0);
+        t = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(1000, Math.log2(1e6))));
+        t.getDescription = (_) => Utils.getMath(getDesc(t.level));
+        t.getInfo = (amount) => Utils.getMathTo(getDesc(t.level), getDesc(t.level + amount));
+        t.maxLevel = 4
     }
 
     // c2
@@ -84,7 +85,7 @@ var updateAvailability = () => {
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
-    currency.value += dt * bonus * getC1(c1.level).pow(getC1Exponent(c1Exp.level)) *
+    currency.value += dt * bonus * getT(c1.level).pow(getC1Exponent(c1Exp.level)) *
                                    getC2(c2.level).pow(getC2Exponent(c2Exp.level));
 }
 
@@ -100,7 +101,7 @@ var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.16
 var getTau = () => currency.value.pow(0.5);
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
-var getC1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
+var getT = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
 var getC2 = (level) => BigNumber.TWO.pow(level);
 var getC1Exponent = (level) => BigNumber.from(1 + 0.05 * level);
 var getC2Exponent = (level) => BigNumber.from(1 + 0.05 * level);
